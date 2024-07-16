18cadeeeb00fb41c3ee7fd3632062e1c4f9a6e75
marouane <marwanmakhloufraja@gmail.com>
2024-07-16 23:01:02 +0100

    add updates

--- js/main.js
+++ js/main.js
@@ -1,20 +1,34 @@
-
 import { items } from "./readmape.js";
+import { skills } from "./skills.js";
 
+// Display the items in the "mape" element
 const ReadMape = document.getElementById('mape');
 
+const showMape = () => {
+  ReadMape.innerHTML = items.map((item, index) => {
+    return `<div class="op"><span>${(index + 1)}</span> ${item.name} <a href="${item.link}">Course</a></div>`;
+  }).join('');
+  ReadMape.innerHTML += `<div class="op"> And more ...</div>`;
+}
 
+showMape();
+
+// Display the skills in the "skill" element
+const skill = document.getElementById('skill');
 
 const showSkills = () => {
-    ReadMape.innerHTML = '';
-    ReadMape.innerHTML = items.map((item, index) => {
-    return `<div class="op"><span>${(index + 1)}</span> ${item.name} <a href="${item.link}">Course</a></div>`
-}).join('');
-    ReadMape.innerHTML +=  `<div class="op"> And more ...</div>`
+  skill.innerHTML = skills.map((item, index) => {
+    return `
+      <div class="skill overflow-hidden" key="${index}">
+        ${item.skill}
+        <span class="skill-cont flex">
+          <span id="_${index}" class="h-full bg-cyan-100 " style="width: ${item.level}%;"></span>
+        </span>
+      </div>`;
+  }).join('');
+
+
 }
+
 showSkills();
 
-// const skill = document.getElementById('skil')
-// for(let i = 0; i<100 ; i++) {
-//     skill.style.backgroundImage = `background-image: linear-gradient(to right , #ffffff ${i}%, transparent );`
-// }
---
+++ js/skills.js
@@ -0,0 +1,16 @@
+export const skills = [
+    { skill: 'Html', level: 90 },
+    { skill: 'Css', level: 90 },
+    { skill: 'JavaScript', level: 85 },
+    { skill: 'TailwindCss', level: 86 },
+    { skill: 'ReactJs', level: 75 },
+    { skill: 'TypeScript', level: 60 },
+    { skill: 'NodeJs', level: 50 },
+    { skill: 'ExpressJs', level: 50 },
+    { skill: 'MongoDb', level: 50 },
+    { skill: 'Figma', level: 70 },
+    { skill: 'git/gitHub', level: 90 },
+    { skill: 'C++', level: 60 },
+    { skill: 'C', level: 60 },
+    { skill: 'Python', level: 60 }
+];
